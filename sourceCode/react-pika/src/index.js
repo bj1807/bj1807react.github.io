a5a0ef44ec93849f3d49bacfc8978b4122767b56
fantaofan <1402395251@qq.com>
2019-08-04 16:17:31 +0800

    依赖

--- sourceCode/react-pika/src/index.js
+++ sourceCode/react-pika/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./login";
+import App from "./shouye";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ sourceCode/react-pika/src/shouye/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./index.scss";
+
+export default class Shouye extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>LOFTER</h1>
+        </header>
+      </div>
+    );
+  }
+}
