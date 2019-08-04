9f1e7870e0bce1ce63ab030f5d77cf86be8f4c7f
fantaofan <1402395251@qq.com>
2019-08-04 15:27:20 +0800

    'rem布局'

---
+++ sourceCode/react-pika/src/login/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./index.scss";
+
+export default class Login extends Component {
+  render() {
+    return <div className="login">登录</div>;
+  }
+}
